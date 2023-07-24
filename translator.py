246bfa940e57022930aa5043a773c2c732b468d8
unknown <randaalrasheed108@gmail.com>
2023-07-24 15:54:07 +0300

    added tranlator.py

---
+++ translator.py
@@ -0,0 +1,23 @@
+import deep_translator
+from deep_translator import MyMemoryTranslator
+def englishToFrench(englishText):
+    language_translator.set_service_url('url')
+
+    frenchText = language_translator.translate(
+        text='Hello, how are you today?',
+        model_id='en-fr').get_result()
+        
+
+    return frenchText
+
+
+def frenchToEnglish(frenchText):
+    #write the code here
+    language_translator.set_service_url('url')
+
+    
+    englishText = language_translator.translate(
+        text='Hello, how are you today?',
+        model_id='en-fr').get_result()
+    print(englishText)
+    return englishText
